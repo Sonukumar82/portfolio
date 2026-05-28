7acb6c13c2f6b6a8b960933c2ef3837eab376a27
Sonu Kumar <sonukumar@Sonus-MacBook-Air.local>
2026-05-28 20:17:41 +0530

    Initial portfolio project

---
+++ backend/server.js
@@ -0,0 +1,128 @@
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const app = express();
+
+/* Middleware */
+
+app.use(cors());
+app.use(express.json());
+
+/* Home Route */
+
+app.get("/", (req, res) => {
+  res.send("Backend Working 🚀");
+});
+
+/* Test Mail Route */
+
+app.get("/testmail", async (req, res) => {
+
+  try {
+
+    const transporter = nodemailer.createTransport({
+
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+
+    });
+
+    await transporter.sendMail({
+
+      from: process.env.EMAIL,
+
+      to: process.env.EMAIL,
+
+      subject: "Test Mail",
+
+      text: "Portfolio backend working 🚀",
+
+    });
+
+    res.send("Mail Sent Successfully ✅");
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.send(error.message);
+
+  }
+
+});
+
+/* Contact Route */
+
+app.post("/contact", async (req, res) => {
+
+  const { name, email, message } = req.body;
+
+  try {
+
+    const transporter = nodemailer.createTransport({
+
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+
+    });
+
+    await transporter.sendMail({
+
+      from: process.env.EMAIL,
+
+      to: process.env.EMAIL,
+
+      replyTo: email,
+
+      subject: `Portfolio Message from ${name}`,
+
+      text: `
+Name: ${name}
+
+Email: ${email}
+
+Message:
+${message}
+      `,
+
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Message sent successfully",
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to send message",
+    });
+
+  }
+
+});
+
+/* Server */
+
+const PORT = 30001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,607 @@
+import profile from "./assets/profile.png";
+import { useState } from "react";
+import { Typewriter } from "react-simple-typewriter";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { motion } from "framer-motion";
+import { FaBars } from "react-icons/fa";
+import axios from "axios";
+import {
+  FaAws,
+  FaDocker,
+  FaReact,
+  FaGithub,
+} from "react-icons/fa";
+
+import {
+  SiKubernetes,
+  SiJenkins,
+  SiPython,
+} from "react-icons/si";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+  name: "",
+  email: "",
+  message: "",
+});
+
+const handleChange = (e) => {
+
+  setFormData({
+    ...formData,
+    [e.target.name]: e.target.value,
+  });
+
+};
+
+const handleSubmit = async (e) => {
+
+  e.preventDefault();
+
+  if (
+    !formData.name ||
+    !formData.email ||
+    !formData.message
+  ) {
+
+    alert("Please fill all fields ⚠️");
+    return;
+  }
+
+  try {
+      setLoading(true);
+    await axios.post(
+      "http://localhost:30001/contact",
+      formData
+    );
+
+    alert("Message Sent Successfully 🚀");
+    setLoading(false);
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+
+  } catch (error) {
+    console.log(error);
+    alert("Failed To Send Message ❌");
+    setLoading(false);
+  }
+
+};
+  const [menuOpen, setMenuOpen] = useState(false);
+const particlesInit = async (main) => {
+  await loadFull(main);
+};
+  const skills = [
+    { icon: <FaAws />, name: "AWS" },
+    { icon: <SiKubernetes />, name: "Kubernetes" },
+    { icon: <FaDocker />, name: "Docker" },
+    { icon: <SiJenkins />, name: "Jenkins" },
+    { icon: <SiPython />, name: "Python" },
+    { icon: <FaReact />, name: "React" },
+    { icon: <FaGithub />, name: "GitHub" },
+  ];
+
+  return (
+    <>
+  <Particles
+    id="tsparticles"
+    init={particlesInit}
+    options={{
+      background: {
+        color: {
+          value: "#050816",
+        },
+      },
+      fpsLimit: 120,
+      particles: {
+        color: {
+          value: "#00ffff",
+        },
+        links: {
+          color: "#00ffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.2,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          speed: 1,
+        },
+        number: {
+          value: 50,
+        },
+        opacity: {
+          value: 0.3,
+        },
+        size: {
+          value: 2,
+        },
+      },
+    }}
+    className="absolute inset-0 -z-10"
+  />
+
+  <div className="relative z-10"></div>
+    
+    <div className="min-h-screen bg-[#050816] text-white overflow-hidden">
+
+      {/* Navbar */}
+      <nav className="fixed top-0 left-0 w-full z-50 backdrop-blur-lg bg-white/5 border-b border-white/10">
+
+  <div className="flex justify-between items-center px-10 py-5">
+
+    <h1 className="text-2xl font-bold text-cyan-400">
+      Sonu.dev
+    </h1>
+
+    <ul className="hidden md:flex gap-8 text-gray-300">
+      <div
+  className="md:hidden text-2xl cursor-pointer"
+  onClick={() => setMenuOpen(!menuOpen)}
+>
+  <FaBars />
+</div>
+      <a href="#home">
+        <li className="hover:text-cyan-400 cursor-pointer transition">
+          Home
+        </li>
+      </a>
+
+      <a href="#skills">
+        <li className="hover:text-cyan-400 cursor-pointer transition">
+          Skills
+        </li>
+      </a>
+
+      <a href="#projects">
+        <li className="hover:text-cyan-400 cursor-pointer transition">
+          Projects
+        </li>
+      </a>
+
+      <a href="#contact">
+        <li className="hover:text-cyan-400 cursor-pointer transition">
+          Contact
+        </li>
+      </a>
+
+    </ul>
+
+  </div>
+
+</nav>
+{
+  menuOpen && (
+    <div className="md:hidden fixed top-20 left-0 w-full bg-[#0b1120] border-b border-gray-800 z-40">
+
+      <ul className="flex flex-col items-center gap-6 py-8 text-lg">
+
+        <a href="#home" onClick={() => setMenuOpen(false)}>
+          <li className="hover:text-cyan-400">
+            Home
+          </li>
+        </a>
+
+        <a href="#skills" onClick={() => setMenuOpen(false)}>
+          <li className="hover:text-cyan-400">
+            Skills
+          </li>
+        </a>
+
+        <a href="#projects" onClick={() => setMenuOpen(false)}>
+          <li className="hover:text-cyan-400">
+            Projects
+          </li>
+        </a>
+
+        <a href="#contact" onClick={() => setMenuOpen(false)}>
+          <li className="hover:text-cyan-400">
+            Contact
+          </li>
+        </a>
+
+      </ul>
+
+    </div>
+  )
+}
+      {/* Hero Section */}
+      <section
+  id="home"
+  className="flex flex-col justify-center items-center text-center min-h-screen pt-28 px-6 relative">
+
+        <div className="absolute w-72 h-72 bg-cyan-500 blur-[120px] opacity-20 rounded-full top-20"></div>
+       <motion.img
+  initial={{ opacity: 0, scale: 0.8 }}
+  animate={{ opacity: 1, scale: 1 }}
+  transition={{ duration: 1 }}
+  src={profile}
+  alt="profile"
+  className="w-36 h-36 md:w-44 md:h-44 rounded-full border-4 border-cyan-400 shadow-lg shadow-cyan-500/50 mb-8 object-cover"
+/>
+        <motion.h2
+          initial={{ opacity: 0, y: -40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="text-4xl sm:text-5xl md:text-7xl font-bold leading-tight"
+        >
+          Hi, I'm{" "}
+          <span className="text-cyan-400">
+            Sonu Kumar
+          </span>
+        </motion.h2>
+
+        <motion.p
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  transition={{ delay: 0.5 }}
+  className="mt-6 text-lg md:text-2xl text-gray-400 max-w-3xl"
+>
+
+  <Typewriter
+    words={[
+      "DevOps Engineer",
+      "AWS Cloud",
+      "Kubernetes Expert",
+      "CI/CD Automation Engineer",
+      "Azure cloud",
+      "GCP Cloud"
+    ]}
+    loop={true}
+    cursor
+    cursorStyle="_"
+    typeSpeed={80}
+    deleteSpeed={50}
+    delaySpeed={1500}
+  />
+
+</motion.p>
+
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          className="mt-10 px-8 py-4 bg-cyan-500 rounded-2xl text-lg font-semibold shadow-lg shadow-cyan-500/50 hover:bg-cyan-400 transition"
+        >
+          Explore Projects
+        </motion.button>
+        <a
+  href="/Sonu_K_CV.pdf"
+  download
+>
+  <button className="mt-5 px-8 py-4 border border-cyan-400 rounded-2xl text-lg font-semibold hover:bg-cyan-400 hover:text-black transition">
+    Download Resume
+  </button>
+</a>
+
+      </section>
+{/* About Section */}
+
+<section id="skills" className="px-10 py-20">
+
+  <h2 className="text-4xl font-bold text-center text-cyan-400 mb-10">
+    About Me
+  </h2>
+
+  <div className="max-w-5xl mx-auto bg-[#111827] border border-gray-800 rounded-3xl p-10 text-gray-300 leading-8 text-lg">
+
+    <p>
+      DevOps Engineer with 3.7 years of experience in building and managing
+      CI/CD pipelines, Kubernetes infrastructure, and AWS cloud solutions.
+    </p>
+
+    <p className="mt-6">
+      Experienced with Jenkins, Argo CD, Docker, Terraform, GitHub,
+      Kubernetes, AWS Lambda, EC2, DynamoDB, and monitoring tools like
+      CloudWatch and New Relic.
+    </p>
+
+    <p className="mt-6">
+      Passionate about automation, cloud-native applications,
+      Infrastructure as Code (IaC), and scalable deployment systems.
+    </p>
+
+  </div>
+
+</section>
+      {/* Skills Section */}
+      <section id="skills" className="px-10 py-20">
+
+        <h2 className="text-4xl font-bold text-center text-cyan-400 mb-14">
+          Skills
+        </h2>
+
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+
+          {skills.map((skill, index) => (
+            <motion.div
+              key={index}
+              whileHover={{ scale: 1.05 }}
+              className="bg-[#111827] border border-gray-800 rounded-2xl p-8 flex flex-col items-center justify-center hover:border-cyan-400 transition"
+            >
+              <div className="text-5xl text-cyan-400 mb-4">
+                {skill.icon}
+              </div>
+
+              <h3 className="text-xl font-semibold">
+                {skill.name}
+              </h3>
+            </motion.div>
+          ))}
+
+        </div>
+
+      </section>
+      {/* Experience Section */}
+
+<section className="px-10 py-20">
+
+  <h2 className="text-4xl font-bold text-center text-cyan-400 mb-14">
+    Experience
+  </h2>
+
+  <div className="max-w-5xl mx-auto space-y-8">
+
+    <div className="bg-[#111827] border border-gray-800 rounded-3xl p-8 hover:border-cyan-400 transition">
+
+      <h3 className="text-2xl font-bold">
+        Senior Software Engineer — Capgemini
+      </h3>
+
+      <p className="text-cyan-400 mt-2">
+        Oct 2024 – Present
+      </p>
+
+      <ul className="mt-5 space-y-3 text-gray-300">
+        <li>• Built CI/CD pipelines using Jenkins and Argo CD</li>
+        <li>• Managed Kubernetes clusters and cloud-native apps</li>
+        <li>• Worked on AWS services like Lambda, EC2, DynamoDB</li>
+        <li>• Automated infrastructure using Terraform</li>
+      </ul>
+
+    </div>
+
+    <div className="bg-[#111827] border border-gray-800 rounded-3xl p-8 hover:border-cyan-400 transition">
+
+      <h3 className="text-2xl font-bold">
+        Software Engineer — Capgemini
+      </h3>
+
+      <p className="text-cyan-400 mt-2">
+        Dec 2022 – Oct 2024
+      </p>
+
+      <ul className="mt-5 space-y-3 text-gray-300">
+        <li>• Worked on DevOps and Java Full Stack technologies</li>
+        <li>• Developed CI/CD pipelines for GCP deployments</li>
+        <li>• Managed GitHub repositories and AWS infrastructure</li>
+        <li>• Worked with React, Microservices, and MySQL</li>
+      </ul>
+
+    </div>
+
+  </div>
+
+</section>
+{/* Projects Section */}
+
+<section id="projects" className="px-10 py-20">
+
+  <h2 className="text-4xl font-bold text-center text-cyan-400 mb-14">
+    Projects
+  </h2>
+
+  <div className="grid md:grid-cols-3 gap-8">
+
+    {/* Project 1 */}
+    <div className="bg-[#111827] border border-gray-800 rounded-3xl p-8 hover:border-cyan-400 transition hover:scale-105 duration-300">
+
+      <h3 className="text-2xl font-bold mb-4">
+        Kubernetes CI/CD Pipeline
+      </h3>
+
+      <p className="text-gray-400 leading-7">
+        Built automated deployment pipelines using Jenkins, Argo CD,
+        Docker, and Kubernetes with GitOps workflow.
+      </p>
+
+    </div>
+
+    {/* Project 2 */}
+    <div className="bg-[#111827] border border-gray-800 rounded-3xl p-8 hover:border-cyan-400 transition hover:scale-105 duration-300">
+
+      <h3 className="text-2xl font-bold mb-4">
+        AWS Cloud Infrastructure
+      </h3>
+
+      <p className="text-gray-400 leading-7">
+        Designed scalable cloud infrastructure using EC2, Lambda,
+        DynamoDB, S3, and CloudWatch monitoring solutions.
+      </p>
+
+    </div>
+
+    {/* Project 3 */}
+    <div className="bg-[#111827] border border-gray-800 rounded-3xl p-8 hover:border-cyan-400 transition hover:scale-105 duration-300">
+
+      <h3 className="text-2xl font-bold mb-4">
+        Terraform Automation
+      </h3>
+
+      <p className="text-gray-400 leading-7">
+        Automated infrastructure provisioning using Terraform and GitHub
+        workflows for faster deployment and scalability.
+      </p>
+
+    </div>
+
+  </div>
+
+</section>
+{/* GitHub Section */}
+
+<section className="px-10 py-20">
+
+  <h2 className="text-4xl font-bold text-center text-cyan-400 mb-14">
+    GitHub
+  </h2>
+
+  <div className="grid md:grid-cols-3 gap-8">
+
+    {/* Repo 1 */}
+    <a
+      href="https://github.com/Sonukumar82"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div className="bg-[#111827] border border-gray-800 rounded-3xl p-8 hover:border-cyan-400 transition hover:scale-105 duration-300">
+
+        <h3 className="text-2xl font-bold mb-4">
+          DevOps Projects
+        </h3>
+
+        <p className="text-gray-400 leading-7">
+          Kubernetes, Jenkins, Docker, Terraform, AWS and CI/CD related projects.
+        </p>
+
+      </div>
+    </a>
+
+    {/* Repo 2 */}
+    <a
+      href="https://github.com/Sonukumar82"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div className="bg-[#111827] border border-gray-800 rounded-3xl p-8 hover:border-cyan-400 transition hover:scale-105 duration-300">
+
+        <h3 className="text-2xl font-bold mb-4">
+          React Applications
+        </h3>
+
+        <p className="text-gray-400 leading-7">
+          Frontend applications using React, JavaScript, APIs, and responsive UI.
+        </p>
+
+      </div>
+    </a>
+
+    {/* Repo 3 */}
+    <a
+      href="https://github.com/Sonukumar82"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div className="bg-[#111827] border border-gray-800 rounded-3xl p-8 hover:border-cyan-400 transition hover:scale-105 duration-300">
+
+        <h3 className="text-2xl font-bold mb-4">
+          Cloud Automation
+        </h3>
+
+        <p className="text-gray-400 leading-7">
+          AWS automation scripts, Infrastructure as Code, monitoring, and deployment workflows.
+        </p>
+
+      </div>
+    </a>
+
+  </div>
+
+</section>
+{/* Certifications */}
+
+<section className="px-10 py-20">
+
+  <h2 className="text-4xl font-bold text-center text-cyan-400 mb-14">
+    Certifications
+  </h2>
+
+  <div className="grid md:grid-cols-2 gap-8 max-w-5xl mx-auto">
+
+    <div className="bg-[#111827] p-6 rounded-2xl border border-gray-800">
+      AWS Cloud Practitioner
+    </div>
+
+    <div className="bg-[#111827] p-6 rounded-2xl border border-gray-800">
+      Microsoft Azure Fundamentals
+    </div>
+
+    <div className="bg-[#111827] p-6 rounded-2xl border border-gray-800">
+      Google Associate Cloud Engineer
+    </div>
+
+    <div className="bg-[#111827] p-6 rounded-2xl border border-gray-800">
+      DevOps Beginner to Advanced
+    </div>
+
+  </div>
+
+</section>
+{/* Contact Section */}
+
+<section
+  id="contact"
+  className="px-10 py-20 text-center"
+>
+
+  <h2 className="text-4xl font-bold text-cyan-400 mb-10">
+    Contact Me
+  </h2>
+
+  <form
+  onSubmit={handleSubmit}
+  className="max-w-3xl mx-auto flex flex-col gap-6"
+>
+
+    <input
+  type="text"
+  name="name"
+  value={formData.name}
+  onChange={handleChange}
+  placeholder="Your Name"
+  className="bg-[#111827] border border-gray-700 rounded-2xl px-6 py-4 outline-none"
+/>
+
+    <input
+  type="email"
+  name="email"
+  value={formData.email}
+  onChange={handleChange}
+  placeholder="Your Email"
+  className="bg-[#111827] border border-gray-700 rounded-2xl px-6 py-4 outline-none"
+/>
+
+    <textarea
+  name="message"
+  value={formData.message}
+  onChange={handleChange}
+  placeholder="Your Message"
+  rows="6"
+  className="bg-[#111827] border border-gray-700 rounded-2xl px-6 py-4 outline-none"
+></textarea>
+
+<button
+  type="submit"
+  className="bg-cyan-500 hover:bg-cyan-400 transition rounded-2xl py-4 text-lg font-semibold"
+>
+
+  {
+    loading
+      ? "Sending..."
+      : "Send Message"
+  }
+
+</button>
+  </form>
+
+</section>
+    </div>
+    </>
+  );
+}
+
+export default App;
